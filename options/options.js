5b700c40a48b3cd10a1cc607fecca4ee6e0f7beb
Crowdbotics <team@crowdbotics.com>
2024-03-04 19:26:19 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "crimson-poetry-46797",
+  url: "https://crimson-poetry-46797.botics.co",
+  api: "https://crimson-poetry-46797.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
